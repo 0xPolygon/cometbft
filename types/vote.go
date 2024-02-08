b274f1f21723c1d20b7e4ce5763f6aea332df4a3
Raneet Debnath <raneetdebnath10@gmail.com>
2024-02-08 16:51:36 +0530

    types: fix TestInvalidPrecommitExtensions

--- types/vote.go
+++ types/vote.go
@@ -327,9 +327,9 @@ func (vote *Vote) ValidateBasic() error {
 		// It's possible that this vote has vote extensions but
 		// they could also be disabled and thus not present thus
 		// we can't do all checks
-		// if len(vote.ExtensionSignature) > MaxSignatureSize {
-		// 	return fmt.Errorf("vote extension signature is too big (max: %d)", MaxSignatureSize)
-		// }
+		if len(vote.ExtensionSignature) > MaxSignatureSize {
+			return fmt.Errorf("vote extension signature is too big (max: %d)", MaxSignatureSize)
+		}
 
 		// NOTE: extended votes should have a signature regardless of
 		// of whether there is any data in the extension or not however
