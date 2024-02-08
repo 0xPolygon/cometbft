399632850264b9e02ba652bcd99bfa95656a5bdd
Raneet Debnath <raneetdebnath10@gmail.com>
2024-02-08 16:32:14 +0530

    types: fix TestBlockMaxDataBytesNoEvidence

--- types/block_test.go
+++ types/block_test.go
@@ -497,8 +497,8 @@ func TestBlockMaxDataBytesNoEvidence(t *testing.T) {
 		0: {-10, 1, true, 0},
 		1: {10, 1, true, 0},
 		2: {841, 1, true, 0},
-		3: {842, 1, false, 0},
-		4: {843, 1, false, 1},
+		3: {843, 1, false, 0},
+		4: {844, 1, false, 1},
 	}
 
 	for i, tc := range testCases {
